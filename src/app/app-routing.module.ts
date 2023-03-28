e39708a9fe92684abbd6025ceb6b063c2fccdb4a
JoseManuel88 <113127826+JoseManuel88@users.noreply.github.com>
2023-03-28 20:34:52 +0200

    Commit: Funcionalidad de juego piedra,papel, tijeras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  // {
+  //   path: '',
+  //   // loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  //   component: AppComponent
+  // }
 ];
 @NgModule({
   imports: [
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  jugando: Boolean = false;
+
+  resultado = false;
+
+  victoria = false;
+
+  posibilidades = ['Piedra', 'Papel', 'Tijeras'];
+
+  jugadaAleatoria: String = '';
+
+  jugadaElegida: String = '';
+
+  resultadoCompeticion: String = '';
+
+  constructor() {
+  }
+
+  jugar() {
+    this.jugando = true;
+    console.log(this.posibilidades[this.getRandomInt()]);
+  }
+
+  elegir(escogido: String) {
+    this.jugadaElegida = escogido;
+    this.jugadaAleatoria = this.posibilidades[this.getRandomInt()];
+    console.log(this.jugadaAleatoria);
+    this.resultado = true;
+    this.jugando = false;
+
+    if (escogido === 'Piedra'
+    && this.jugadaAleatoria === 'Tijeras') {
+      this.resultadoCompeticion = 'Victoria!';
+    } else if (escogido === 'Papel'
+    && this.jugadaAleatoria === 'Piedra') {
+      this.resultadoCompeticion = 'Victoria!';
+    } else if (escogido === 'Tijeras'
+    && this.jugadaAleatoria === 'Papel') {
+      this.resultadoCompeticion = 'Victoria!';
+    }
+     else if (escogido === this.jugadaAleatoria) {
+      this.resultadoCompeticion = 'Empate!';
+    }
+    else {
+      this.resultadoCompeticion = 'Derrota!';
+    }
+  }
+
+  getRandomInt() {
+    return Math.floor(Math.random() * this.posibilidades.length);
+  }
 }
